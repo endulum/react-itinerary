2a25ecbd354306376b60372d5bef4dfeeb3d872f
endulum <71853916+endulum@users.noreply.github.com>
2023-11-03 18:45:58 -0700

    new setup, basic readme details

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Creating a To-Do list in React.
+## React Itinerary
+
+Exploration of React's features through a To-Do application.
+
+### Goals
+* [ ] Render itinerary data
+* [ ] Store itinerary data locally whenever it changes
+* [ ] Use a reducer to manage stateful itinerary data
+* [ ] Use Context to deeply deliver itinerary data
+* [ ] Use Router to navigate between itinerary lists as pages
+
+### To Consider
+- Styling
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { DataProvider } from './components/Context';
-
-import Lists from './components/Lists';
-import List from './components/List';
-
-const getList = (id) => {
-  const data = JSON.parse(localStorage.getItem('data'));
-  const found = data.filter((list) => list.id === id);
-  return found;
-};
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Lists />,
-  }, {
-    path: 'lists/:listId',
-    element: <List />,
-    loader: ({ params }) => {
-      const list = getList(params.listId);
-      return { list };
-    },
-  },
-]);
-
 export default function App() {
   return (
-    <DataProvider>
-      <RouterProvider router={router} />
-    </DataProvider>
+    <h1>Hello World</h1>
   );
 }
